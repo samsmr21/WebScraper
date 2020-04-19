dd488d5045ab5a06119dc54bbf6329b2f0f6a936
vaibhavsharma <samsmr21@gmail.com>
2020-04-19 14:35:40 +0100

    project setup

---
+++ src/main/java/com/sainsbury/Application.java
@@ -0,0 +1,12 @@
+package com.sainsbury;
+
+import java.util.logging.Logger;
+
+public class Application {
+
+  private static final Logger logger = Logger.getLogger(Application.class.getName());
+
+  public static void main(String[] args) {
+
+  }
+}
